c50798a1d2f5fd464dc25ae727559178a5a183be
q <q@localhost.localdomain>
2018-06-30 10:47:20 -0400

    Updated path file for linux using relative path of dataset.

--- recaman/Recaman ML/plot_Recaman.py
+++ recaman/Recaman ML/plot_Recaman.py
@@ -6,7 +6,7 @@ import numpy as np
 recamanList = list()
 xValues = list()
 
-with open('C:\\Users\\DRock\\Documents\\GitHub\\marvel\\recaman\\Recaman ML\\Datasets\\RecamanTrainingData_30000') as f:
+with open('Datasets/RecamanTrainingData_30000') as f:
     lines = f.readlines()
 
     for line in lines:
@@ -21,8 +21,3 @@ plt.plot(xValues,recamanList)
 plt.ylabel('Recaman Output (labels)')
 plt.xlabel('Step')
 plt.show()
-
-
-
-
-
