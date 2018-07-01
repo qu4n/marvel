d9bda0be138d2af14dc353be33e6cef080162a2f
D-wr3ck <derrick.baptiste@gmail.com>
2018-07-01 16:33:42 -0400

    Updates

--- recaman/Recaman ML/RecamanNN.py
+++ recaman/Recaman ML/RecamanNN.py
@@ -1,87 +1,125 @@
-from __future__ import absolute_import, division, print_function
+from __future__ import print_function
 
-import os
-import matplotlib.pyplot as plt
+import math
 
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
 import tensorflow as tf
-import tensorflow.contrib.eager as tfe
-
-tf.enable_eager_execution()
-
-print("TensorFlow version: {}".format(tf.VERSION))
-print("Eager execution: {}".format(tf.executing_eagerly()))
+from tensorflow.python.data import Dataset
 
 
-def parse_csv(line):
-  example_defaults = [[0], [0]]  # sets field types
-  parsed_line = tf.decode_csv(line, example_defaults)
-  # First field is the feature, combine into single tensor
-  feature = tf.reshape(parsed_line[:-1], shape=(1,))
-  # Last field is the label
-  label = tf.reshape(parsed_line[-1], shape=())
-  return feature, label
+#Loading our dataset
+recaman_sequence_dataframe = pd.read_csv(".\\Datasets\\RecamanTrainingData_10000.csv", sep=",")
 
-def loss(model, x, y):
-  y_ = model(x)
-  return tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_)
+#Randomize the data
+recaman_sequence_dataframe = recaman_sequence_dataframe.reindex(np.random.permutation(recaman_sequence_dataframe.index))
 
-def grad(model, inputs, targets):
-  with tf.GradientTape() as tape:
-    loss_value = loss(model, inputs, targets)
-  return tape.gradient(loss_value, model.variables)
+#Below line calculates statistics on input data contained in the .csv file
+#print(recaman_sequence_dataframe.describe())
 
-#Specify path to where training data file resides
-train_dataset_rs = ".\\Datasets\\RecamanTrainingData_30000.csv"
+#Define the input feature: steps.
+my_feature = recaman_sequence_dataframe[["step"]]
 
-train_dataset = tf.data.TextLineDataset(train_dataset_rs)
-train_dataset = train_dataset.map(parse_csv)      # parse each row
-train_dataset = train_dataset.shuffle(buffer_size=1000)  # randomize
-train_dataset = train_dataset.batch(32)
+#Configure a numberic feature column for total steps.
+feature_columns = [tf.feature_column.numeric_column("step")]
 
-# View a single example entry from a batch
-feature, label = iter(train_dataset).next()
-print("\nexample features:", feature[0])
-print("\nexample label:", label[0])
+#Define the label.
+targets = recaman_sequence_dataframe["recaman_output_value"]
 
-model = tf.keras.Sequential([
-  tf.keras.layers.Dense(10, activation="relu", input_shape=(1,)),  # input shape required
-  tf.keras.layers.Dense(10, activation="relu"),
-  tf.keras.layers.Dense(3)
-])
+#Use gradient descent as the optimizer for training the model.
+my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+# Configure the linear regression model with our feature columns and optimizer.
+# Set a learning rate of 0.01 for Gradient Descent.
+linear_regressor = tf.estimator.LinearRegressor(
+    feature_columns=feature_columns,
+    optimizer=my_optimizer
+)
 
-## Note: Rerunning this cell uses the same model variables
-
-''' Under Contruction
-# keep results for plotting
-train_loss_results = []
-train_accuracy_results = []
-
-num_epochs = 201
-
-for epoch in range(num_epochs):
-  epoch_loss_avg = tf.metrics.
-  epoch_accuracy = tf.metrics.accuracy()
-
-  # Training loop - using batches of 32
-  for x, y in train_dataset:
-    # Optimize the model
-    grads = grad(model, x, y)
-    optimizer.apply_gradients(zip(grads, model.variables),
-                              global_step=tf.train.get_or_create_global_step())
-
-    # Track progress
-    epoch_loss_avg(loss(model, x, y))  # add current batch loss
-    # compare predicted label to actual label
-    epoch_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32), y)
-
-  # end epoch
-  train_loss_results.append(epoch_loss_avg.result())
-  train_accuracy_results.append(epoch_accuracy.result())
+def my_input_fn(features, targets, batch_size=10, shuffle=True, num_epochs=None):
+    """Trains a linear regression model of one feature.
   
-  if epoch % 50 == 0:
-    print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
-                                                                epoch_loss_avg.result(),
-                                                                epoch_accuracy.result()))
-'''
+    Args:
+      features: pandas DataFrame of features
+      targets: pandas DataFrame of targets
+      batch_size: Size of batches to be passed to the model
+      shuffle: True or False. Whether to shuffle the data.
+      num_epochs: Number of epochs for which data should be repeated. None = repeat indefinitely
+    Returns:
+      Tuple of (features, labels) for next data batch
+    """
+  
+    # Convert pandas data into a dict of np arrays.
+    features = {key:np.array(value) for key,value in dict(features).items()}                                           
+ 
+    # Construct a dataset, and configure batching/repeating.
+    ds = Dataset.from_tensor_slices((features,targets)) # warning: 2GB limit
+    ds = ds.batch(batch_size).repeat(num_epochs)
+    
+    # Shuffle the data, if specified.
+    if shuffle:
+      ds = ds.shuffle(buffer_size=10000)
+    
+    # Return the next batch of data.
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
+
+_ = linear_regressor.train(
+    input_fn = lambda:my_input_fn(my_feature, targets),
+    steps=100
+)
+
+# Create an input function for predictions.
+# Note: Since we're making just one prediction for each example, we don't 
+# need to repeat or shuffle the data here.
+prediction_input_fn =lambda: my_input_fn(my_feature, targets, num_epochs=1, shuffle=False)
+
+# Call predict() on the linear_regressor to make predictions.
+predictions = linear_regressor.predict(input_fn=prediction_input_fn)
+
+# Format predictions as a NumPy array, so we can calculate error metrics.
+predictions = np.array([item['predictions'][0] for item in predictions])
+
+# Print Mean Squared Error and Root Mean Squared Error.
+mean_squared_error = metrics.mean_squared_error(predictions, targets)
+root_mean_squared_error = math.sqrt(mean_squared_error)
+print("Mean Squared Error (on training data): %0.3f" % mean_squared_error)
+print("Root Mean Squared Error (on training data): %0.3f" % root_mean_squared_error)
+
+calibration_data = pd.DataFrame()
+calibration_data["predictions"] = pd.Series(predictions)
+calibration_data["targets"] = pd.Series(targets)
+print(calibration_data.describe())
+
+sample = recaman_sequence_dataframe.sample(n=300)
+
+# Get the min and max step values.
+x_0 = sample["step"].min()
+x_1 = sample["step"].max()
+
+# Retrieve the final weight and bias generated during training.
+weight = linear_regressor.get_variable_value('linear/linear_model/step/weights')[0]
+bias = linear_regressor.get_variable_value('linear/linear_model/bias_weights')
+
+# Get the predicted median_house_values for the min and max total_rooms values.
+y_0 = weight * x_0 + bias 
+y_1 = weight * x_1 + bias
+
+# Plot our regression line from (x_0, y_0) to (x_1, y_1).
+plt.plot([x_0, x_1], [y_0, y_1], c='r')
+
+# Label the graph axes.
+plt.ylabel("recaman_output_value")
+plt.xlabel("step")
+
+# Plot a scatter plot from our data sample.
+plt.scatter(sample["step"], sample["recaman_output_value"])
+
+# Display graph.
+plt.show()
